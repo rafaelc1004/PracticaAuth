cd00353e082c6b59a9a98527e5b3d365cfb718fc
rafaelc1004/ <rafael_30_08@hotmail.com>
2024-07-27 16:44:27 -0400

    Iniciando Projecto

--- Autentication/user/apps.py
+++ Autentication/user/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AutenticarConfig(AppConfig):
+class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'autenticar'
+    name = 'user'
